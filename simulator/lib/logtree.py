27f88cc42d36586bdfd4f6cd7a03695a9bd8a3c8
Thibaut Guinoiseau <thibaut.guinoiseau@gmail.com>
2026-01-24 11:33:57 +0100

    logtree: fix

--- simulator/lib/logtree.py
+++ simulator/lib/logtree.py
@@ -1,14 +1,15 @@
 import re
 import sys
 
-TREE_BRANCH="\u251c" # ├
-TREE_FINAL_BRANCH= "\u2514" # └
-TREE_INTERMEDIATE_LINE="\u252c" # ┬
-TREE_DASH="\u2500" # ─
+TREE_BRANCH = "\u251c"  # ├
+TREE_FINAL_BRANCH = "\u2514"  # └
+TREE_INTERMEDIATE_LINE = "\u252c"  # ┬
+TREE_DASH = "\u2500"  # ─
 
-TREE_INDENT_SIZE=2
+TREE_INDENT_SIZE = 4
 
-ID_PATTERN = re.compile(r"#(\.[\w+-]+)*")
+ID_PATTERN = re.compile(r"#\d*(\.[\w+-]+)*")
+
 
 class LogTree:
     def __init__(self, id, log):
@@ -47,7 +48,7 @@ class LogTree:
 
         return self
 
-    def print(self, indent="", writer=print):
+    def print_tree(self, indent="", writer=print):
         last_id_fragment = self.id.split('.')[-1]
         writer(f"{indent}{last_id_fragment} : {self.logs[0]}")
         for idx, log in enumerate(self.logs[1:]):
@@ -56,25 +57,44 @@ class LogTree:
 
         for idx, child in enumerate(self.children):
             # last = idx == len(self.children) - 1
-            child.print(indent + (" "*TREE_INDENT_SIZE), writer)
+            child.print(indent + (" " * TREE_INDENT_SIZE), writer)
 
+    def find_and_print(self, log_id, log, indent="", writer=print):
+        if log_id == self.id:
+            last_id_fragment = self.id.split('.')[-1]
+            writer(f"{indent}{last_id_fragment} : {log}")
+        elif self.id in log_id:
+            matching_child = None
+            for (index, child) in enumerate(self.children):
+                if child.id in log_id:
+                    matching_child = child
+                    break
+            matching_child.find_and_print(log_id, log, indent + (" " * TREE_INDENT_SIZE), writer)
+        else:
+            writer(f"Not found {log_id} : got as far as {self.id}")
 
 
 def parse_logs(logs: list[str]) -> LogTree:
-    tree = None
+    tree = LogTree("", "init")
     for log in [l[:-1] for l in logs]:
-        id_match = ID_PATTERN.search(log)
-        if id_match is None:
-            print(f"Not a tree log: {log}")
+        log_id = parse_id(log)
+        if log_id is None:
+            # print(f"Not a tree log: {log}")
             continue
-        log_id = id_match.group(0)
-        print(f"log_id: {log_id}")
+        # print(f"log_id: {log_id}")
         if tree is None:
             tree = LogTree(log_id, log)
         else:
             tree = tree.insert(log_id, log)
     return tree
 
+
+def parse_id(log):
+    id_match = ID_PATTERN.search(log)
+    if id_match is not None:
+        return id_match.group(0)
+    else:
+        return None
 
 
 def main():
@@ -84,7 +104,10 @@ def main():
         logs = f.readlines()
         log_tree = parse_logs(logs)
         if log_tree is not None:
-            log_tree.print()
+            for log in [l[:-1] for l in logs]:
+                log_id = parse_id(log)
+                if log_id is not None and log_id != "":
+                    log_tree.find_and_print(log_id, log)
         else:
             print(f"No tree log found in {filename}")
 
